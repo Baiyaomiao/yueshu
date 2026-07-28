dc701544132a7e603680b6945af9e098d92b1fa5
Baiyaomiao <dev@example.com>
2026-07-28 14:17:56 +0800

    Capacitor Android project

--- android/app/src/main/java/ys/reader/MainActivity.java
+++ android/app/src/main/java/ys/reader/MainActivity.java
@@ -1,28 +1,5 @@
 package ys.reader;
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebSettings;
-import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity {
-    private WebView wv;
-    @Override
-    protected void onCreate(Bundle b) {
-        super.onCreate(b);
-        wv = new WebView(this);
-        WebSettings s = wv.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setAllowFileAccess(true);
-        s.setDomStorageEnabled(true);
-        s.setDatabaseEnabled(true);
-        s.setCacheMode(WebSettings.LOAD_DEFAULT);
-        wv.setWebViewClient(new WebViewClient());
-        wv.loadUrl("file:///android_asset/index.html");
-        setContentView(wv);
-    }
-    @Override
-    public void onBackPressed() {
-        if (wv.canGoBack()) wv.goBack();
-        else super.onBackPressed();
-    }
-}
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
